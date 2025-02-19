239456e4b4231fa56fb12bfcc5a175cec31aa9a7
YoussefMohammed001 <yoseffmohamed001@gmail.com>
2025-02-19 21:33:30 +0200

    S3

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,136 @@
-import 'package:flutter/material.dart';
+import 'package:codex_flutter_g1/user_info.dart';
 
 void main() {
+//   for(int num = 0; num <= 10 ; num ++){
+//     print(num);
+// if(num == 5){
+//  break;
+// }
+//
+//   }
+// for(int num = 0; num <= 10;num++){
+//   print(num);
+//   if(num == 5){
+//    continue;
+//   }
 
-  runApp(const MyApp());
+// int num = 0;
+// do {
+//   num++;
+//   print(num);
+// }while(num < 10);
+
+// List<int> numbers = [1,2,3,4,5];
+// for(int number in numbers){
+//   print(number);
+// }
+
+// List<User> users = [
+//   User("ahmed",20),
+//   User("ahmed",21),
+//   User("ahmed",23),
+//   User("ahmed",25),
+//   User("ahmed",30),
+// ];
+
+// for(var element in users){
+//   print(element.age);
+// }
+
+// var user =  "ahmed";
+// user = "mohamed";
+//
+//
+// dynamic salary = 2000;
+// salary = "20000";
+// salary = 3000.300;
+// salary = true;
+
+// printName("Ahmed");
+// printName("Mohamed");
+// printName("Ali");
+
+// getSum(10,20);
+//   getSum(num1: 10,num2: 30);
+//   print(sum);
+
+//print(  getSum(num2: 1,num1: 1));
+
+// print(getSum(num1: 10, num2: 20));
+
+
+
+// to define obj ===>        ClassName objName = ClassName();
+// UserInfo userInfo = UserInfo();
+// UserInfo userInfo2 = UserInfo();
+// print(UserInfo.course);// => static (class level)
+// userInfo.age = 20;
+// userInfo2.age = 25;
+// print(userInfo.age);
+// print(userInfo2.age);
+
+ UserInfo userInfo = UserInfo(name: 'ahmed');
+// UserInfo userInfo2 = UserInfo(name: 'ali');
+// print(userInfo.name);
+// print(userInfo2.name);
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// to call function ===> printName();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+  // dataType functionName(param1,param2,param3){
+  // //code
+  // }
 
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+//  printName(String name){
+//   print(name);
+// }
+
+// getSum(int num1, [int num2 = 0]){
+//   int sum = num1 + num2;
+//   print(" $num1  + $num2 = $sum");
+// }
+
+//
+//  late int sum;
+// getSum({required int num1, int num2 = 0}){
+//      sum = num1 + num2;
+//    print(" $num1  + $num2 = $sum");
+// }
+
+
+// default req + named req
+// getSum(int num1, [int num2 = 0]){
+//  int sum = num1 + num2;
+//     print(" $num1  + $num2 = $sum");
+// }
 
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+// parameters
+ // ==> Default parameters  =>  Default required parameters, Default optional parameters
+ // ==> Named parameters => Named required parameters, Named optional parameters
 
-  final String title;
+ // String getSum({required int num1, required int num2 }){
+ //  int sum = num1 + num2;
+ //  int multi = num1 * num2;
+ //  return  "sum = $sum \n multi = $multi";
+ // }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//  List<String> getSum({required int num1, required int num2}){
+//     int sum = num1 + num2;
+//      int multi = num1 * num2;
+//
+// return   [
+//   "sum = $sum",
+//   "multi = $multi",
+//
+// ];
+ //}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+
+
---
+++ lib/user_info.dart
@@ -0,0 +1,11 @@
+class UserInfo {
+  // static  const  course = "Flutter";
+  // String name = "ahmed";
+  // late String phoneNumber;
+  // late int age;
+
+  final   String  name;
+  UserInfo({required this.name,});
+
+}
+
