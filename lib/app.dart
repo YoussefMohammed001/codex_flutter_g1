3047d667f89c204d1ba1aae9cb48a1fd52c139ff
YoussefMohammed001 <yoseffmohamed001@gmail.com>
2025-02-26 22:27:46 +0200

    s5 part 2

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:codex_flutter_g1/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold => appBar, body,bottom navigation bar,floating, drawer
+    // Column => vertical list of widgets
+    // Row => horizontal list of widgets
+    return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () {
+              print("pressed");
+              counter--;
+              setState(() {
+
+              });
+            },
+          ),
+          SizedBox(
+          width:30,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              print("pressed");
+              counter++;
+              setState(() {
+
+              });
+            },
+          ),
+          SizedBox(
+            width:30,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.restart_alt),
+            onPressed: () {
+              print("pressed");
+              counter++;
+              setState(() {
+
+              });
+            },
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Home Screen",
+        style:TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w700
+        ) ,)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+                children: [
+        Text("Home Screen",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+        ),
+               Text(counter.toString()),
+
+
+                ],
+              ),
+      ),
+    );
+  }
+}
