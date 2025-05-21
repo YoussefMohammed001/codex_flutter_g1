f2fd613ca1ea643c68db34381ea2f22ca875a6a0
YoussefMohammed001 <yoseffmohamed001@gmail.com>
2025-05-21 16:14:04 +0300

    session 25

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -7,7 +7,6 @@ import 'package:get_it/get_it.dart';
 final  getIt = GetIt.instance;
 
 Future<void> setupGetIt() async{
-
   // TOP MOVIES
   getIt.registerLazySingleton<TopMoviesUsecase>(() => TopMoviesUsecase(topMoviesRepository:getIt()));
   getIt.registerLazySingleton<TopMoviesRepository>(() => TopMoviesRepositoryImp(topMoviesApi:getIt()));
